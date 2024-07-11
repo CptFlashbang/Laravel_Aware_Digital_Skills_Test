ddb27e7422a87d11112a26a24c4c2f9198497466
CptFlashbang <Callum.yates.180@gmail.com>
2024-07-11 22:18:15 +0100

    Add import

--- app/Http/Controllers/JokeController.php
+++ app/Http/Controllers/JokeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Joke;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class JokeController extends Controller
 {
