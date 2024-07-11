cd60a59e1bd50d650da122a6b773a202732103e1
CptFlashbang <Callum.yates.180@gmail.com>
2024-07-11 21:59:02 +0100

    Add route for joke indexing

--- app/Http/Controllers/JokeController.php
+++ app/Http/Controllers/JokeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Joke;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class JokeController extends Controller
@@ -10,9 +11,9 @@ class JokeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('jokes.index');
     }
 
     /**
